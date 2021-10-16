e0b84b313dd954424595c215776197ce02da47c8
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-10-16 16:15:29 +0300

    Update retele_conv_seg.py

--- retele_conv_seg.py
+++ retele_conv_seg.py
@@ -10,6 +10,9 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
 
+
+
+
 from UNetModel import UNetModel
 config = None
 with open('config.yaml') as f: # reads .yml/.yaml files
@@ -186,7 +189,7 @@ axs[1].imshow(y[0], cmap="gray")
 unet = UNetModel()
 # n_channels=1, deoarece imaginea de input are un singur canal
 # n_classes=1, o singura clasa de prezis -> plaman vs background
-unet_model = unet.build(*config["data"]["img_size"], n_channels=1, n_classes=1)
+unet_model = unet.build(*config["data"]["img_size"], n_channels=3, n_classes=3)
 unet_model.summary()
 
 
@@ -197,7 +200,67 @@ valid_df =dataset_df.loc[dataset_df['subset']=='valid']# de completat
 valid_gen = LungSegDataGenerator(valid_df, img_size=config["data"]["img_size"], batch_size=config["train"]["bs"], shuffle=True)
 
 
+
 unet_model.compile(loss="binary_crossentropy",optimizer=tf.keras.optimizers.Adam(learning_rate=config['train']['lr']) , metrics=["accuracy"])
 
-callbacks = [keras.callbacks.ModelCheckpoint('damn.h5', save_best_only=True)]
-history=unet_model.fit(train_gen ,steps_per_epoch=10, epochs=config['train']['epochs'], validation_data=valid_gen , callbacks=callbacks)
+callbacks = [
+    keras.callbacks.ModelCheckpoint('damn.h5', save_best_only=True)
+    ]
+history=unet_model.fit(train_gen ,batch_size=8 , validation_data=valid_gen , epochs=config['train']['epochs'],callbacks=callbacks,workers=1)
+
+def plot_acc_loss(result):
+    acc = result.history['acc']
+    loss = result.history['loss']
+    val_acc = result.history['val_acc']
+    val_loss = result.history['val_loss']
+    
+    plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    plt.plot(acc, label='Train')
+    plt.plot(val_acc, label='Validation')
+    plt.title('Accuracy', size=15)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    
+    plt.subplot(122)
+    plt.plot(loss, label='Train')
+    plt.plot(val_loss, label='Validation')
+    plt.title('Loss', size=15)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    
+    plt.show()
+    
+plot_acc_loss(history)
+
+test_df = dataset_df.loc[dataset_df['subset']=='test']# de completat
+test_gen = LungSegDataGenerator(test_df, img_size=config["data"]["img_size"], batch_size=config["train"]["bs"], shuffle=False)
+result = unet_model.evaluate(test_gen)
+print(f"Test Acc: {result[1] * 100}")
+
+x, y = test_gen[0]
+y_pred = unet_model.predict(x)
+y_pred.shape
+
+r_exs = 4 # nr de exemple de afisat
+fig, axs = plt.subplots(nr_exs, 3, figsize=(10, 10))
+for i, (img, gt, pred) in enumerate(zip(x[:nr_exs], y[:nr_exs], y_pred[:nr_exs])):
+    axs[i][0].axis('off')
+    axs[i][0].set_title('Input')
+    axs[i][0].imshow(img, cmap='gray')
+
+    axs[i][1].axis('off')
+    axs[i][1].set_title('Ground truth')
+    axs[i][1].imshow(gt, cmap='gray')
+
+    pred[pred > config['test']['threshold']] = 1
+    pred[pred <= config['test']['threshold']] = 0
+    pred = pred.astype("uint8")
+
+    axs[i][2].axis('off')
+    axs[i][2].set_title('Prediction')
+    axs[i][2].imshow(pred, cmap='gray')
