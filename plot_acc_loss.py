e886cf1cac9f3c7661536987c1fea165bdd08a5d
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-11-15 14:20:42 +0200

    Create plot_acc_loss.py
    move plot_acc_loss

---
+++ plot_acc_loss.py
@@ -0,0 +1,27 @@
+def plot_acc_loss(result):
+    acc = result.history['accuracy']
+    loss = result.history['loss']
+    val_acc = result.history['val_accuracy']
+    val_loss = result.history['val_loss']
+    
+    plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    plt.plot(acc, label='Train')
+    plt.plot(val_acc, label='Validation')
+    plt.title('Accuracy', size=15)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    
+    plt.subplot(122)
+    plt.plot(loss, label='Train')
+    plt.plot(val_loss, label='Validation')
+    plt.title('Loss', size=15)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    
+    plt.show()
+
