f7e71df0e77163f759c3dbd603c493714ef00f67
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-12-02 18:54:38 +0200

    Update plot_acc_loss.py
    Dice index afisare

--- plot_acc_loss.py
+++ plot_acc_loss.py
@@ -2,19 +2,19 @@ import matplotlib.pyplot as plt
 
 def plot_acc_loss(result):
 
-    acc = result.history['accuracy']
+    acc = result.history['dice_coef']
     loss = result.history['loss']
-    val_acc = result.history['val_accuracy']
+    val_acc = result.history['val_dice_coef']
     val_loss = result.history['val_loss']
     
     plt.figure(figsize=(15, 5))
     plt.subplot(121)
     plt.plot(acc, label='Train')
     plt.plot(val_acc, label='Validation')
-    plt.title('Accuracy', size=15)
+    plt.title('Dice Index', size=15)
     plt.legend()
     plt.grid(True)
-    plt.ylabel('Accuracy')
+    plt.ylabel('Dice Index')
     plt.xlabel('Epoch')
     
     plt.subplot(122)
